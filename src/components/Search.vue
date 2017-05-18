ea12c49ab4367e70529581b4589733c3837f0493
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-05-18 19:59:19 -0300

    Adding search to app

---
+++ src/components/Search.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <div> <!-- ⚠️ Este div es importante: todos los componentes deben tener un unico elemento principal -->
+    <input type="text" v-model="query">
+    <button type="button" @click="search">Search</button>
+
+    <ul>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+
+  data () {
+    return {
+      query: '',
+      results: []
+    }
+  },
+
+  methods: {
+    search () {
+      console.log(this.query)
+    }
+  }
+}
+</script>
