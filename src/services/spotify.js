9694e87407df843f347fb67e2144952fcd967b94
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-05-18 17:53:13 -0300

    Adding spotify service

--- src/services/spotify.js
+++ src/services/spotify.js
@@ -1,4 +1,28 @@
-const spotifyService = {}
+const spotifyService = {
+  // Esta es la url base de la API
+  baseUrl: 'https://api.spotify.com/v1'
+}
 
+spotifyService.search = function (query, type) {
+  // Usando template strings armamos la url del endpoint con
+  // el formato esperado por la API.
+  const url = `${this.baseUrl}/search?q=${query}&type=${type}`
+
+  // Retornamos la llamada de fetch (recuerden que devuelve una Promise).
+  // Fetch recibe como primer parametro la url y luego un objeto
+  // con configuracion donde pasamos el tipo de metodo HTTP
+  return fetch(url, { method: 'GET' })
+    .then(res => {
+      // En el caso de que hay un error disparamos un excpecio
+      // para luego manejarla en nuesrtro componenten
+      if (res.status !== 200) {
+        throw res.statusText
+      }
+
+      // Casteamos la respuesta y la devolvemos para
+      // usarla luego en el componente
+      return res.json()
+    })
+}
 
 export default spotifyService
